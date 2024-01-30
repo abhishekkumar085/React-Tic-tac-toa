5a4eb4a782c67eae0e030846f4495c5425e740cc
abhishekkumar085 <abhishekkumar08528@gmail.com>
2024-01-30 08:29:37 +0530

    Code Complete

---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Card from '../Card/Card';
+import './Grid.css';
+import isWinner from '../../helpers/checkWinner';
+
+function Grid({ numberOfCards }) {
+  const [board, setBoard] = useState(Array(numberOfCards).fill(''));
+  const [turn, setTurn] = useState(true);
+  const [winner, setWinner] = useState(null);
+  function play(index) {
+    if (turn == true) {
+      board[index] = 'O';
+    } else {
+      board[index] = 'X';
+    }
+
+    const win = isWinner(board, turn ? 'o' : 'x');
+
+    if (win) {
+      setWinner(win);
+    }
+    setBoard([...board]);
+    setTurn(!turn);
+  }
+
+  function reset() {
+    setTurn(true);
+    setWinner(null);
+    setBoard(Array(numberOfCards).fill(''));
+  }
+  return (
+    <div className="grid-wrapper">
+      {winner && (
+        <>
+          <h1 className="turn-highlight">Winner is {winner}</h1>
+          <button className="reset" onClick={reset}>
+            Reset Game
+          </button>
+        </>
+      )
+    }
+
+      <h1 className="turn-highlight">Current turn :{turn ? 'O' : 'X'}</h1>
+
+      <div className="grid">
+        {board.map((elm, idx) => (
+          <Card key={idx} onPlay={play} player={elm} index={idx} />
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Grid;
